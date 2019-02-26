bb086514cff2fce9d8a346ce7f5cb7870b10704d
Hakan Fouren <hakan@vilynx.com>
2019-02-27 04:20:51 +0800

    Add saltenv whitelist to git_pillar repos
    This help to workaround an issue with dynamic git_pillar branches
    by allowing us to only use them for feature development branches,
    thus reducing the probability of git branches colliding when
    applying state to multiple minions that use different pillarenv's
    
    Essentially we create separate git_pillar repos for each "official"
    branch (master, test, dev, qa, etc.) and then only use the dynamic
    feature for branches that will only be deployed when forcing the
    pillarenv or saltenv on the command line (and praying that only 1
    such deployment command will run at any given time...)

--- salt/utils/gitfs.py
+++ salt/utils/gitfs.py
@@ -21,6 +21,7 @@ import time
 import tornado.ioloop
 import weakref
 from datetime import datetime
+import re
 
 # Import salt libs
 import salt.utils.configparser
@@ -2994,6 +2995,17 @@ class GitPillar(GitBase):
                     env = repo.env
                 else:
                     env = 'base' if repo.branch == repo.base else repo.get_checkout_target()
+                # If dynamic repo has a whitelist, then make sure the branch matches
+                if repo.branch == '__env__' and hasattr(repo, 'saltenv_whitelist'):
+                    if any(re.match(pat + '$', env) for pat in repo.saltenv_whitelist):
+                        log.debug(
+                            'env \'%s\' matches dynamic repo whitelist \'%s\'',
+                            env, repo.saltenv_whitelist)
+                    else:
+                        log.debug(
+                            'env \'%s\' fails dynamic repo whitelist \'%s\', skipping',
+                            env, repo.saltenv_whitelist)
+                        continue
                 if repo._mountpoint:
                     if self.link_mountpoint(repo):
                         self.pillar_dirs[repo.linkdir] = env
